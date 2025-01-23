674af582d8e25867a36f7d8ab2570fdd5766bfbe
Mauricio Rojas <mrojasb2000@gmail.com>
2025-01-23 12:35:29 -0300

    chore: adding database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [PokemonModule],
+  imports: [
+    PokemonModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+  ],
 })
 export class AppModule {}
