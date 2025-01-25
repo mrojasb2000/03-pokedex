9897b18470aa159a8c62896cca92dca5f2cd1353
Mauricio Rojas <mrojasb2000@gmail.com>
2025-01-24 22:17:22 -0300

    chore: adding update pokemon instance

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -61,8 +61,15 @@ export class PokemonService {
     return pokemon;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+    const pokemon = await this.findOne(term);
+
+    if (updatePokemonDto.name)
+      updatePokemonDto.name = updatePokemonDto.name.toLocaleLowerCase();
+
+    await pokemon.updateOne(updatePokemonDto, { new: true });
+
+    return { ...pokemon.toJSON(), ...updatePokemonDto };
   }
 
   remove(id: number) {
