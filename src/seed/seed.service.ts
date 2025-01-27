003198e94b5db49fe4e678bfae91b655d70416a0
Mauricio Rojas <mrojasb2000@gmail.com>
2025-01-27 12:02:09 -0300

    chore: getting data from pokeapi.co and process data result

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  findAll() {
-    return `This action returns all seed`;
+  private readonly axios: AxiosInstance = axios;
+
+  async findAll() {
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    );
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no = Number(segments[segments.length - 2]);
+      console.log({ name, no });
+    });
+
+    return data.results;
   }
 }
